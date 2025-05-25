480c6ff87730fe3bd2b82302fb873ffbe6eb46da
Rana Saha <ranasaha03edu@gmail.com>
2025-05-25 21:14:00 +0530

    Add Flash Message and modify UI

--- controllers/Front/FileManageController.js
+++ controllers/Front/FileManageController.js
@@ -8,26 +8,27 @@ exports.index = async (req, res) => {
         res.render('index');
     }
     catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal server error' });
+        req.flash('error_msg', error);
+        req.redirect('/');
       } 
 }
 
 exports.upload = async (req, res) => {
     try {
         // Call the file upload helper to handle file upload
-        console.log(req.files[0]);
         const result = await fileUploadHelper.uploadFile(req.files[0], '../public');
 
         if (result.success) {
             // Respond with a success message
-            res.status(200).json({ message: 'File uploaded successfully.', filePath: result.filePath });
+            req.flash('success_msg', 'File uploaded successfully.');
+            res.redirect('/');
         } else {
             // Respond with an error message
-            res.status(500).json({ message: result.message });
+            req.flash('error_msg', result.message);
+            req.redirect('/');
         }
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal server error' });
+        req.flash('error_msg', error);
+        req.redirect('/');
     }
 }
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const multer = require('multer');
 const upload = multer();
+const session = require('express-session');
+const flash = require('connect-flash');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -19,6 +21,21 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(upload.any());
+
+app.use(session({
+  secret: 'yourSecretKey',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(flash());
+
+// Make flash messages available to all views
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  next();
+});
 
 // Define routes
 const userRoutes = require('./routes/front/userRoutes');
